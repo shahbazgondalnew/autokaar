1a05adebe14a40224ac86735fac5ac496c79c04e
F17-9437-Shehroz <125120385+F17-9437-Shehroz@users.noreply.github.com>
2023-12-02 13:42:02 +0500

    Car Log Screen Updated

--- lib/userMod/car_model.dart
+++ lib/userMod/car_model.dart
@@ -2,7 +2,7 @@ class CarModel {
   final String id;
   final String companyName;
   final String modelName;
-
+//Car Class
   CarModel(
       {required this.id, required this.companyName, required this.modelName});
 }
